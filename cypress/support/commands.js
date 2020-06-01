13d507d6afd308a9546e2ce3db812327e3978ea7
Tominous <18189490+Tominous@users.noreply.github.com>
2020-06-01 12:29:36 +0100

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -18,16 +18,16 @@ Cypress.Commands.add('visitAboutPage', () => {
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
+ Cypress.Commands.add("login", (email, password) => { ... })
 //
 //
 // -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+ Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
 //
 //
 // -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+ Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
 //
 //
 // -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+ Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
